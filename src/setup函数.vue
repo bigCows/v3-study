ea21f70d66b8bc8b95fad0af853f7134dc94fbb0
plant <94729798+bigCows@users.noreply.github.com>
2023-02-23 11:16:32 +0800

    Update setup函数.vue

--- src/setup函数.vue
+++ src/setup函数.vue
@@ -10,7 +10,7 @@ export default {
     // T0、在setup中使用onbeforeMount能够拿到index.html中的app，因为该元素是独立于组件之外的容器，组件没创建时，此容器就已经创建了
     // 1、setup在组件没创建之前就执行了，相当于beforCreate之前，所以不存在this指向，this指向undefined
     // 2、setup中定义所有的数据都要返回才能使用
-    // console.logs(this);
+     console.logs(this);
     const num = 2;
     const fn = () => {
       console.log("hello,V3");
